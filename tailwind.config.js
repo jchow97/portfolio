9bdec42177771de24df5394ee8e11c72749b2b40
Jeffrey Chow <72580584+jchow97@users.noreply.github.com>
2023-06-29 17:17:59 -0700

    Added web icon. Fixed preset colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,8 +14,8 @@ export default {
     extend: {
       colors: {
         brightRed: 'hsl(12, 88%, 59%)',
-        brightRedLight: 'hsl(12, 88%, 69%),',
-        brightRedSupLight: 'hsl(12, 88%, 95%),',
+        brightRedLight: 'hsl(12, 88%, 69%)',
+        brightRedSupLight: 'hsl(12, 88%, 95%)',
         darkBlue: 'hsl(228, 39%, 23%)',
         darkGrayishBlue: 'hsl(227, 12%, 61%)',
         veryDarkBlue: 'hsl(233, 12%, 13%)',
