04fe6365b328c9468f3418083d4cc4e4383c4f21
Jeffrey Chow <72580584+jchow97@users.noreply.github.com>
2024-02-24 04:34:09 -0800

    Refactor sections of the page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import NavBar from './components/NavBar';
-import Introduction from './Introduction';
+import Introduction from './sections/Introduction';
 import UnderConstruction from "./UnderConstruction";
 
 function App() {
