e1263b77c8cf235126c898c8fadaa202c83bbfdb
Jeffrey Chow <72580584+jchow97@users.noreply.github.com>
2024-02-24 04:34:28 -0800

    Remove unused imports.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,3 @@
-import Button from "./Button";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
 import {faEnvelope} from "@fortawesome/free-regular-svg-icons";
 import {faFilePdf} from "@fortawesome/free-regular-svg-icons";
