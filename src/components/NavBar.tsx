01d290d4160d19161a819040405dbf90ca47953b
Jeffrey Chow <72580584+jchow97@users.noreply.github.com>
2024-03-01 17:46:39 -0800

    Improved navbar spacing, especially on smaller window sizes. Moved buttons.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,16 +7,18 @@ import Icon from "./Icon";
 function NavBar() {
     return (
         <nav className="container mx-auto p-6">
-            <div className="flex grid grid-cols-3 justify-items-between">
+            <div className="grid grid-cols-4 justify-items-between">
                 <div className={"self-center justify-self-end space-x-6"}>
-                    <Icon icon={faLinkedin} link={"https://www.linkedin.com/in/jeffreychow97/"} />
-                    <Icon icon={faGithub} link={"https://github.com/jchow97"} />
-                    <Icon icon={faEnvelope} link={"mailto:jeffreychow97@gmail.com"} />
-                    <Icon icon={faFilePdf} link={"./JeffreyChowResume_v6.pdf"} />
+                    <Icon icon={faLinkedin} link={"https://www.linkedin.com/in/jeffreychow97/"}/>
+                    <Icon icon={faGithub} link={"https://github.com/jchow97"}/>
                 </div>
-                <h1 className="text-5xl font-bold justify-self-center md:mb-4">
+                <h1 className="text-5xl col-span-2 font-bold justify-self-center md:mb-4">
                     Jeffrey Chow
                 </h1>
+                <div className={"self-center justify-self-start space-x-6"}>
+                    <Icon icon={faEnvelope} link={"mailto:jeffreychow97@gmail.com"}/>
+                    <Icon icon={faFilePdf} link={"./JeffreyChowResume_v6.pdf"}/>
+                </div>
             </div>
         </nav>
     )
