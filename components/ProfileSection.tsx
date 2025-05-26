54aedb474ec265dfec02d8fe8ec61b3fd04db61c
Jeffrey Chow <72580584+jchow97@users.noreply.github.com>
2025-05-25 22:03:01 -0700

    added box-reveal animation

--- components/ProfileSection.tsx
+++ components/ProfileSection.tsx
@@ -1,30 +1,59 @@
+import { BoxReveal } from "@/components/magicui/box-reveal";
+
 function ProfileSection() {
+  const boxRevealColour = "#5046e6";
+  const boxRevealDuration = 1.0;
+  const boxRevealWidth = "fit-content";
+
   return (
     <div className="flex p-4 @container">
       <div className="flex w-full flex-col gap-4 items-center">
         <div className="flex gap-4 flex-col items-center">
+          {/* <BoxReveal
+            boxColor={boxRevealColour}
+            duration={boxRevealDuration}
+            width={boxRevealWidth}
+          > */}
           <div
             className="bg-center bg-no-repeat aspect-square bg-cover rounded-full min-h-32 w-32"
             style={{
-              backgroundImage:
-                'url("images/bookstore_pic.jpeg")',
+              backgroundImage: 'url("images/bookstore_pic.jpeg")',
             }}
           ></div>
+          {/* </BoxReveal> */}
           <div className="flex flex-col items-center justify-center justify-center">
-            <p className="text-white text-[22px] font-bold leading-tight tracking-[-0.015em] text-center">
-              Jeffrey Chow
-            </p>
-            <p className="text-[#9cabba] text-base font-normal leading-normal text-center">
-              Software Developer | Problem Solver | Former Biochemist
-            </p>
+            <BoxReveal
+              boxColor={boxRevealColour}
+              duration={boxRevealDuration}
+              width={boxRevealWidth}
+            >
+              <p className="text-white text-[22px] font-bold leading-tight tracking-[-0.015em] text-center">
+                Jeffrey Chow
+              </p>
+            </BoxReveal>
+            <BoxReveal
+              boxColor={boxRevealColour}
+              duration={boxRevealDuration}
+              width={boxRevealWidth}
+            >
+              <p className="text-[#9cabba] text-base font-normal leading-normal text-center">
+                Software Developer | Problem Solver | Former Biochemist
+              </p>
+            </BoxReveal>
             <br></br>
-            <p className="text-[#9cabba] text-base font-normal leading-normal text-center">
-              Driven full-stack developer with a passion for building impactful
-              software. With experience in web development, serverless AWS apps,
-              and a growing focus on cybersecurity and biotech, I thrive at the
-              intersection of creativity and problem-solving. Let’s turn bold
-              ideas into scalable solutions.
-            </p>
+            <BoxReveal
+              boxColor={boxRevealColour}
+              duration={boxRevealDuration}
+              width={boxRevealWidth}
+            >
+              <p className="text-[#9cabba] text-base font-normal leading-normal text-center">
+                Driven full-stack developer with a passion for building
+                impactful software. With experience in web development,
+                serverless AWS apps, and a growing focus on cybersecurity and
+                biotech, I thrive at the intersection of creativity and
+                problem-solving. Let’s turn bold ideas into scalable solutions.
+              </p>
+            </BoxReveal>
           </div>
         </div>
       </div>
