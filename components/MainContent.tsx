b2571d272c176bfb4b510e1781dfa1fe2b20519c
Jeffrey Chow <72580584+jchow97@users.noreply.github.com>
2025-06-11 16:06:13 -0600

    use dynamic padding to make page more mobile-friendly

--- components/MainContent.tsx
+++ components/MainContent.tsx
@@ -3,8 +3,8 @@ import ProjectsSection from "./ProjectsSection";
 
 function MainContent() {
   return (
-    <div className="px-40 flex flex-1 justify-center py-5">
-      <div className="layout-content-container flex flex-col max-w-[960px] flex-1">
+    <div className="px-4 md:px-10 lg:px-40 flex flex-1 justify-center py-5">
+      <div className="layout-content-container flex flex-col w-full md:max-w-[960px] flex-1">
         <ProfileSection />
         <ProjectsSection />
       </div>
